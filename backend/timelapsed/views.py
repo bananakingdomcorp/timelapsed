733ed8a5662111ca10d1eba5d7e7262a7515851b
Samuel Davis <davisss64@gmail.com>
2019-03-21 13:32:59 -0700

    Added basic Views

--- backend/timelapsed/views.py
+++ backend/timelapsed/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import viewsets 
 
 # Create your views here.
+
+from .models import Users, Topic, Event, Date_Range, Card
+
+from .serializers import UsersSerializer, TopicSerializer, EventSerializer, DateRangeSerializer, CardSerializer
+
+class UsersView(viewsets.ModelViewSet):
+  serializer_class: UsersSerializer
+  model: Users
+
+class TopicView(viewsets.ModelViewSet):
+  serializer_class: TopicSerializer
+  model: Topic
+
+class EventView(viewsets.ModelViewSet):
+  serializer_class: EventSerializer
+  model: Event
+
+class DateRangeView(viewsets.ModelViewSet):
+  serializer_class: DateRangeSerializer
+  model: Date_Range
+
+class CardView(viewsets.ModelViewSet):
+  serializer_class: CardSerializer
+  model: Card
