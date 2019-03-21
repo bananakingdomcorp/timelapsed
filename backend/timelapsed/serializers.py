a216e00552aa05a387188c058d3ff8c44668010e
Samuel Davis <davisss64@gmail.com>
2019-03-21 11:38:06 -0700

    Started Serializers

--- backend/timelapsed/serializers.py
+++ backend/timelapsed/serializers.py
@@ -1,2 +1,31 @@
 # Serialize to/from JSON here
 
+from rest_framework import serializers
+
+from .models import Users, Topic, Event, Date_Range, Card
+
+class UsersSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Users
+    fields = ('id', 'Email')
+
+
+class CardSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Card
+    fields = ('id','User', 'Topic', 'Name', 'Description', 'Position', 'Expected_Finish')
+
+class TopicSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Topic
+    fields =  ('id','User', 'Name', 'Position')
+
+class EventSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Event
+    fields = ('id', 'User')
+
+class DateRangeSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Date_Range
+    fields = ('id', 'User', 'Event_ID', 'Card_ID', 'Date', 'Begin_Time', 'End_Time')
