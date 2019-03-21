067b483641e8adae9e5bd38bafe578e03dfaf1b4
Samuel Davis <davisss64@gmail.com>
2019-03-21 14:28:12 -0700

    Added some URL routes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from timelapsed import views
+
+router = routers.DefaultRouter()    
+
+router.register(r'user', views.UsersView)
+router.register(r'topic', views.TopicView)
+router.register(r'card', views.CardView)
+router.register(r'event', views.EventView)
+router.register(r'date', views.DateRangeView)
+
+"""
+Note, all of the routes that are mentioned here may or may not be used, they are just here for now so that I can test that the work. 
+
+"""
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  path('api/', include(router.urls))     
 ]
