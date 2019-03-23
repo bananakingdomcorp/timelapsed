f50357e9dd229896c995366fdac37914984491ed
Samuel Davis <davisss64@gmail.com>
2019-03-23 02:37:10 -0700

    added for FE oauth, need to fix auth routes

--- Frontend/src/containers/app/index.js
+++ Frontend/src/containers/app/index.js
@@ -2,11 +2,34 @@ import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import BackGroundPage from './backGroundPage'
 import { hot } from 'react-hot-loader'
+import { GoogleLogin } from 'react-google-login';
 
-const App = () => (
-  <div className = 'background'>
-    <BackGroundPage />
-  </div>
-)
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  
+  responseGoogle = (response) => {
+    console.log(response);
+  }
+
+  render() {
+    return (
+      <div className = 'background'>
+      <GoogleLogin
+        clientId="346085700873-dsg2gea98rmschm8f1pm4kt8u0f9av3f.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={this.responseGoogle}
+        onFailure={this.responseGoogle}
+      />,  
+        <BackGroundPage />
+      </div>
+    
+    )
+  }
+
+
+}
 
 export default hot(module)(App)
