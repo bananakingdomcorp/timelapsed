9ea9c3844500036f8d784bbed7c10b83163785d1
Samuel Davis <davisss64@gmail.com>
2019-03-27 17:50:42 -0700

    fixed to add positional arguments

--- backend/timelapsed/services.py
+++ backend/timelapsed/services.py
@@ -4,13 +4,14 @@ from .models import Users, Topic, Event, Date_Range, Card
 
 def get_user_information(data):
 
-  test = [i for i in Topic.objects.values('Name', 'id').filter(Email = data['Email']).order_by('Position') ]
+  test = [i for i in Topic.objects.values('Name', 'id', 'Position').filter(Email = data['Email']).order_by('Position') ]
 
   obj = {}
-  
+
+
   for i in test:
-    cards = [i for i in Card.objects.values('Name', 'Description', 'Expected_Finish' ).filter(Topic = i['id']).order_by('Position')]
-    obj[i['Name']] = cards
+    cards = [i for i in Card.objects.values('Name', 'Description', 'Expected_Finish', 'id' ).filter(Topic = i['id']).order_by('Position')]
+    obj[str(i)] = cards
 
   return obj
 
