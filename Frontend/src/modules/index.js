8ce3d92cf8410ce69909ad05c52f975636398ebc
Samuel Davis <davisss64@gmail.com>
2019-05-18 17:09:17 -0700

    setting up subclassing and relationships

--- Frontend/src/modules/index.js
+++ Frontend/src/modules/index.js
@@ -2,6 +2,8 @@ import { combineReducers } from 'redux'
 import board from './board'
 import token from './token'
 import card from './card'
+import relationships from './relationships'
+
 
 export default combineReducers({
   board,
--- Frontend/src/modules/sublclass.js
+++ Frontend/src/modules/sublclass.js
@@ -1,6 +1,3 @@
-// I chose to represent our relationships between cards here as the board module was getting quite large and bloated. 
-
-//You can have either a relationship or a subclass, not both. 
 
 
 
