e44bc769cc866b400053f8b232ef51d3cd05e805
Samuel Davis <davisss64@gmail.com>
2019-05-29 09:59:48 -0700

    moving everything to get

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY',)
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ['CLIENT_ID']
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ['CLIENT_SECRET']
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ.get('CLIENT_ID',)
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ.get('CLIENT_SECRET',)
 
 # import backend.secret_key as secret
 
