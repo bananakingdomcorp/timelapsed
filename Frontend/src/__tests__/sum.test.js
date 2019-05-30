e1a8b5d29d8cdf5c93c4ea45dad3b1b88e9a174e
Samuel Davis <davisss64@gmail.com>
2019-05-29 18:38:28 -0700

    jest tests work

--- Frontend/src/__tests__/sum.test.js
+++ Frontend/src/__tests__/sum.test.js
@@ -1,6 +1,7 @@
 //Literally just a file to confirm that Jest is working
 
-const sum = require('./sum');
+
+import sum from  './../sum'
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
