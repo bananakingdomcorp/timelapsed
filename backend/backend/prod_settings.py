840c0a470790ea131449fbf06bda3ef72f99e5b5
Samuel Davis <davisss64@gmail.com>
2019-05-29 17:11:48 -0700

    set up circleci postgres

--- backend/backend/prod_settings.py
+++ backend/backend/prod_settings.py
@@ -103,14 +103,13 @@ AUTHENTICATION_BACKENDS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'timelapsed',
-        'USER': 'davis',
-        'PASSWORD': 'test',
-        'HOST': 'localhost',
+        'NAME': 'timelapsed_test',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
