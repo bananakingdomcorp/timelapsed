cf25bbc950b3ac95a1c9f5368a625066b27c0fda
Samuel Davis <davisss64@gmail.com>
2019-05-31 16:38:14 -0700

    added db test back in

--- backend/timelapsed/tests_removed/tests_models.py
+++ backend/timelapsed/tests_removed/tests_models.py
@@ -1,20 +1,21 @@
 ### For Database testing
 
-# from django.test import TestCase
+from django.test import TestCase
 
-# from ..models import Users, Topic, Date_Range, Card, Subclass, Card_Relationships, Topic_Relationships,  Subclass_Relationships
+from ..models import Users, Topic, Date_Range, Card, Subclass, Card_Relationships, Topic_Relationships,  Subclass_Relationships
 
-# class DatabaseProperlyConnectedCheck(TestCase):
+class DatabaseProperlyConnectedCheck(TestCase):
 
-# ## This test is just a sanity check to ensure that our database is connected and properly recieving requests. 
+## This test is just a sanity check to ensure that our database is connected and properly recieving requests. 
 
-#   def setUp(self):
+  def setUp(self):
 
-#     Users.objects.create(Email = '12345@hello.com')
+    Users.objects.create(Email = '12345@hello.com')
 
-#   def test_user_created(self):
-#     new_user = Users.objects.values('Email').get(Email = '12345@hello.com')
+  def test_user_created(self):
+    new_user = Users.objects.values('Email').get(Email = '12345@hello.com')
 
-#     self.assertEqual(new_user['Email'], '12345@hello.com')
+    self.assertEqual(new_user['Email'], '12345@hello.com')
+
 
 
