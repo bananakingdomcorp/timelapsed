7c06651c91a840493a06749ee5dd64e3dd7dc709
Samuel Davis <davisss64@gmail.com>
2019-06-04 11:38:44 -0700

    fixed user views

--- backend/timelapsed/tests/test_responses.py
+++ backend/timelapsed/tests/test_responses.py
@@ -2,6 +2,7 @@ from rest_framework.test import APITestCase
 
 from django.contrib.auth.models import User
 
+from ..models import Users, Topic, Date_Range, Card, Subclass, Card_Relationships, Topic_Relationships,  Subclass_Relationships
 
 class TestUsersResponses(APITestCase):
 
@@ -18,6 +19,26 @@ class TestUsersResponses(APITestCase):
     #Runs after every test
     pass
 
-  def test_if_works(self):
-    response = self.client.get('/api/subclass/')
+  def test_if_rejects_get(self):
+    response = self.client.get('/api/user/')
+    self.assertEqual(response.status_code, 405)
+  
+  def test_if_rejects_put(self):
+    response = self.client.put('/api/user/')
+    self.assertEqual(response.status_code, 405)
+
+  def test_if_rejects_delete(self):
+    response = self.client.delete('/api/user/')
+    self.assertEqual(response.status_code, 405)
+
+  def test_if_accepts_post(self):
+    response = self.client.post('/api/user/', {'Email' : 'Test@test.com'})
+    self.assertEqual(response.status_code, 201)
+
+  def test_if_returns_200_when_valid(self):
+    Users.objects.create(Email = 'Test@test.com')
+    self.client.post('/api/user/', {'Email' : 'Test@test.com'}, format = 'json')    
+    response = self.client.post('/api/user/', {'Email' : 'Test@test.com'}, format = 'json')
     self.assertEqual(response.status_code, 200)
+
+  
