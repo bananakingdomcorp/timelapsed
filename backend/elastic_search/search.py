4a060e6b61bbb80c65fc1f2d7ba5248b0d186d8c
Samuel Davis <davisss64@gmail.com>
2019-06-19 11:37:40 -0700

    added es mappings for cards

--- backend/elastic_search/search.py
+++ backend/elastic_search/search.py
@@ -1,7 +1,37 @@
 from elasticsearch_dsl.connections import connections
-from elasticsearch_dsl import DocType, Text, Date, Search
+from elasticsearch_dsl import Search
 from elasticsearch.helpers import bulk
 from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
 
+from elasticsearch_dsl import Document, Date, Nested, Integer, Boolean, \
+    analyzer, InnerDoc, Completion, Keyword, Text
+import datetime
 
 connections.create_connection(alias='elastic_search', hosts=['localhost'], timeout=60)
+
+client = Elasticsearch()
+
+s = Search(using=client)
+
+
+class Card(Document):
+  id = Integer()
+  Name = Text()
+  Name_Suggest = Completion()
+  Description = Text()
+  Description_Suggest = Completion()
+  Topic = Text()
+  Topic_Suggest = Completion()
+
+  category = Text(
+    analyzer='snowball',
+    fields={'raw': Keyword()}
+  )
+
+  class Index:
+     name = 'Timelapsed'
+
+  def save(self, ** kwargs):
+      return super().save(** kwargs)
+
