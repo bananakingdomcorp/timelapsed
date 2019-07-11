b1707d61d380ec757e618c8b7aa03ad4b639195e
Samuel Davis <davisss64@gmail.com>
2019-07-10 18:42:59 -0700

    broke out signal defs

--- backend/timelapsed/signals/signals.py
+++ backend/timelapsed/signals/signals.py
@@ -16,29 +16,30 @@ from django.dispatch import receiver
 
 #Move
 
+def perform_card_relationship_lookup(relationship):
+  for i in relationship:
+    parent_actions =Card_Relationship_Parent_Action.objects.filter(Move_ID = i)
+    for j in parent_actions:
+      try:
+        child_action = Card_Relationship_Child_Action(Parent_Action = j)
+        services.peform_child_action(child_action)
+      except Card_Relationship_Child_Action.DoesNotExist:
+          continue   
+
+
+
 @receiver(pre_save, sender = Card)
 def move_signal(sender, instance, *args, **kwargs):
 
 
   if instance.id != None:
 
-
     instance_in_DB = Card.objects.get(id = instance.id)
-
 
     if instance_in_DB.Topic != instance.Topic:
 
-      relationships =  Card_Relationship_Move_Action.objects.filter(Card_ID = instance, Topic_ID = instance.Topic )
-      for i in relationships:
-        parent_actions =Card_Relationship_Parent_Action.objects.filter(Move_ID = i)
-        for j in parent_actions:
-          try:
-              child_action = Card_Relationship_Child_Action(Parent_Action = j)
-              services.peform_child_action(child_action)
-          except Card_Relationship_Child_Action.DoesNotExist:
-              continue       
-  else:
-    pass
+      perform_card_relationship_lookup( Card_Relationship_Move_Action.objects.filter(Card_ID = instance, Topic_ID = instance.Topic ))
+
 
 
 #Delete
@@ -46,24 +47,16 @@ def move_signal(sender, instance, *args, **kwargs):
 @receiver(pre_delete, sender = Card)
 def delete_signal(sender, instance, **kwargs):
 
-  relationships = Card_Relationship_Delete_Action.objects.filter(Card_ID = instance)
-  for i in relationships:
-    parent_actions =Card_Relationship_Parent_Action.objects.filter(Move_ID = i)
-    for j in parent_actions:
-      try:
-          child_action = Card_Relationship_Child_Action(Parent_Action = j)
-          services.peform_child_action(child_action)
-      except Card_Relationship_Child_Action.DoesNotExist:
-          continue     
-
+  perform_card_relationship_lookup(Card_Relationship_Delete_Action.objects.filter(Card_ID = instance))
 
 
 #Subclass
 
 @receiver(pre_save, sender = Subclass_Relationships)
 def subclass_signal(sender, instance, **kwargs):
-  # Card_Relationship_Subclass_Action
 
-  pass
+
+  perform_card_relationship_lookup( Card_Relationship_Subclass_Action.objects.filter(Subclass_ID = instance.Subclass, Card_ID = instance.Child_ID))
+
 
 #Tag
